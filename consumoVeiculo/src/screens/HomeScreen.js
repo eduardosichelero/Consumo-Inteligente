4cbbf064e51409b1dbd818f9f450b99bdc038d4c
Eduardo da Silva Sichelero <135671476+eduardosichelero@users.noreply.github.com>
2024-09-01 16:33:29 -0300

    feat: Visual e tabela de consumo atualizado

--- consumoVeiculo/src/screens/HomeScreen.js
+++ consumoVeiculo/src/screens/HomeScreen.js
@@ -12,29 +12,73 @@ const HomeScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Quilometragem (KM):</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        value={km}
-        onChangeText={setKm}
-      />
-      <Text style={styles.label}>Litros de Gasolina:</Text>
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        value={litros}
-        onChangeText={setLitros}
-      />
-      <Button title="Calcular" onPress={calcularMedia} />
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>Calculadora de Consumo de Combustível</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>Quilometragem (KM):</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          value={km}
+          onChangeText={setKm}
+        />
+        <Text style={styles.label}>Litros de Gasolina:</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          value={litros}
+          onChangeText={setLitros}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Calcular"
+          onPress={calcularMedia}
+          color="#007bff" // Cor do botão (ajuste conforme necessário)
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 20, justifyContent: 'center' },
-  label: { fontSize: 18, marginBottom: 8 },
-  input: { borderWidth: 1, padding: 10, marginBottom: 20, fontSize: 18 },
+  container: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  headerContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  inputContainer: {
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    marginBottom: 20,
+    fontSize: 18,
+    width: '100%',
+    borderRadius: 10,
+    backgroundColor: '#f5f5f5',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
 });
 
 export default HomeScreen;
--- consumoVeiculo/src/screens/ResultScreen.js
+++ consumoVeiculo/src/screens/ResultScreen.js
@@ -1,46 +1,93 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
-import FuelConsumptionChart from '../components/FuelConsumptionChart';
+import FuelConsumptionTable from '../components/FuelConsumptionTable';
 
 const ResultScreen = ({ route, navigation }) => {
   const { media } = route.params;
 
   const classifyConsumption = () => {
-    if (media > 12) return 'A - Excelente consumo';
-    if (media <= 12 && media > 10) return 'B - Bom consumo';
-    if (media <= 10 && media > 8) return 'C - Consumo razoável';
-    if (media <= 8 && media > 4) return 'D - Consumo baixo';
-    return 'E - Consumo muito baixo';
+    if (media > 12) return 'A';
+    if (media <= 12 && media > 10) return 'B';
+    if (media <= 10 && media > 8) return 'C';
+    if (media <= 8 && media > 4) return 'D';
+    return 'E';
   };
+
+  const highlight = classifyConsumption();
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Resultado</Text>
-      <Text style={styles.text}>Média de Consumo: {media.toFixed(2)} Km/L</Text>
-      <Text style={styles.text}>Classificação: {classifyConsumption()}</Text>
-      <Button title="Voltar" onPress={() => navigation.goBack()} />
-        
+      <View style={styles.tableContainer}>
+        <FuelConsumptionTable highlight={highlight} />
+      </View>
+      <View style={styles.resultContainer}>
+        <Text style={styles.title}>Resultado</Text>
+        <Text style={styles.text}>Média de Consumo: {media.toFixed(2)} Km/L</Text>
+        <Text style={styles.text}>Classificação: {highlight} - {getClassificationText(highlight)}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Voltar" 
+          onPress={() => navigation.goBack()} 
+          color="#007bff" // Cor do botão (ajuste conforme necessário)
+        />
+      </View>
     </View>
-
-
   );
 };
 
+const getClassificationText = (classification) => {
+  switch (classification) {
+    case 'A': return 'Excelente consumo';
+    case 'B': return 'Bom consumo';
+    case 'C': return 'Consumo razoável';
+    case 'D': return 'Consumo baixo';
+    case 'E': return 'Consumo muito baixo';
+    default: return '';
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 2,
-    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 15,
+  },
+  tableContainer: {
     alignItems: 'center',
-    padding: 20,
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+  },
+  resultContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+    padding: 15,
+    backgroundColor: '#f5f5f5',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
   },
   title: {
     fontSize: 24,
-    marginBottom: 20,
+    fontWeight: '700',
+    
   },
   text: {
     fontSize: 18,
     marginVertical: 10,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    padding: 15,
+    alignItems: 'center',
   },
 });
 
